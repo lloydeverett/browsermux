e947ef8dcd6ba02aaf805c1cbba0ff50041b4723
Lloyd Everett <lloydeverett@gmail.com>
2025-09-07 18:44:22 +0200

    Initial Commit

---
+++ BrowserMux/BrowserMuxApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrowserMuxApp.swift
+//  BrowserMux
+//
+//  Created by Lloyd Everett on 2025/09/07.
+//
+
+import SwiftUI
+
+@main
+struct BrowserMuxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrowserMux/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BrowserMux
+//
+//  Created by Lloyd Everett on 2025/09/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
